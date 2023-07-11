31b4a6d5d43c0fd44f7ecc48486a6cf3f9d38e4a
YggdrasilJL <yggdrasiljl@gmail.com>
2023-07-10 21:01:49 -0300

    finish miniproject (kinda)

--- 28-Stu_Mini-Project/Develop/routes/diagnostics.js
+++ 28-Stu_Mini-Project/Develop/routes/diagnostics.js
@@ -4,12 +4,20 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
-  // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json')
+  .then((data) => res.json(data))
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
-  // TODO: Logic for appending data to the db/diagnostics.json file
+  const { errors } = req.body
+  const newDiag = {
+    errors,
+    error_id: uuidv4(),
+    time: Date.now()
+  }
+  readAndAppend(newDiag, './db/diagnostics.json')
+  res.json('Diagnostic added.')
 });
 
 module.exports = diagnostics;
